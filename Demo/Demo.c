2c1b8fa4a1bf8ba875ed8cdcf3300caa99f14230
MinhLoc8423 <minhlocworkemail@gmail.com>
2021-11-13 14:40:05 +0700

    Demo

--- Demo/Demo.c
+++ Demo/Demo.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
 int main() {
-
-    printf("Hello world \n");
-    return 0;
-
+    int a[7] = {3,1,52,2,9,7,10};
+    int i,j,temp;
+    for(i = 0; i<= 6;i++){
+        for(j =i+1;j <= 6;j++){
+            if(a[j] < a[i]){
+                temp = a[i];
+                a[j] = a[i];
+                a[i] = temp;
+            }
+        }
+    }
+    for(int k=0;k <= 6;k++){
+        printf("%d ",a[k]);
+    }
 }
