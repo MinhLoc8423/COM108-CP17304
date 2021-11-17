7f992d5bc8e89a632530d8d4372af67ae1903a0a
MinhLoc8423 <minhlocworkemail@gmail.com>
2021-11-17 23:18:21 +0700

    sort

--- Demo/Demo.c
+++ Demo/Demo.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int a[7] = {3,1,52,2,9,7,10};
-    int i,j,temp;
-    for(i = 0; i<= 6;i++){
-        for(j =i+1;j <= 6;j++){
-            if(a[j] < a[i]){
-                temp = a[i];
-                a[j] = a[i];
-                a[i] = temp;
+    int a[20]={2,3,8,5,1,7,10};
+    for(int i = 0; i < 7 - 1; i++){
+        for(int j = i + 1; j < 7; j++){
+            if(a[i] < a[j]){
+                // Hoan vi 2 so a[i] va a[j]
+                int tg;
+                tg = a[i];
+                a[i] = a[j];
+                a[j] = tg;        
             }
         }
     }
-    for(int k=0;k <= 6;k++){
-        printf("%d ",a[k]);
+    for (int i = 0; i < 7; i++)
+    {
+        printf("%d",a[i]);
     }
+    
 }
